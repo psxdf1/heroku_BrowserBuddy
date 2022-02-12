5aede919fff3acefc678dce3b344fe07ce6bc4ea
psxdf1 <psxdf1@nottingham.ac.uk>
2022-02-12 18:59:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -70,8 +70,8 @@ def index():
 @app.route('/request', methods=["POST"])
 def add_guide():
     #title = request.json['title']
-    if("content" in request.json):
-         content = request.json['content']
+    # if("content" in request.json):
+    #      content = request.json['content']
     print(request.json)
     url=request.json['url']
     url=base64.b64decode(url).decode("utf-8")
